776253144e6165ed0d03a7d53c59debcfd9d79e5
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-09 18:04:48 -0300

    Create calculadora.kt

---
+++ calculadora.kt
@@ -0,0 +1,18 @@
+class calculadora(val a: Int, val b: Int){
+    
+    fun somar(a: Int, b: Int):Int{
+        return a + b
+    }
+    
+    fun subtrair(a: Int, b: Int):Int{
+        return a - b
+    }
+    
+    fun mutiplicar(a: Int, b: Int):Int{
+        return a * b
+    }
+    
+    fun dividir(a: Int, b: Int):Int{
+        return a / b
+    }
+}
