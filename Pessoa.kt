02195ab5232bbff04dfa47f6c8e2f97d91570164
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-09 16:59:48 -0300

    Create Pessoa.kt

---
+++ Pessoa.kt
@@ -0,0 +1,6 @@
+
+class Pessoa(val nome: String, val altura: Double, val idade: Int){
+        fun apresentar(){
+            println("Meu nome é $nome, minha altura é $altura, minha idade é $idade")
+        }
+}
