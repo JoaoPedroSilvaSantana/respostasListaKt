dec6165acf11feb7474bc7051c9aff561ea4c858
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-13 17:17:14 -0300

    Create retangulo.kt

---
+++ retangulo.kt
@@ -0,0 +1,9 @@
+class retangulo(val base: Double, val altura: Double){
+	fun calcularArea (base: Double, altura: Double):Double{
+        return base / altura
+    }
+    
+    fun calcularPerimetro (base: Double, altura: Double):Double{
+        return base / altura * 2
+    }
+}
