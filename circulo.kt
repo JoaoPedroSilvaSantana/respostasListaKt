82a2425b5eb440137aa72d534a5143771be86281
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-13 17:23:26 -0300

    Create circulo.kt

---
+++ circulo.kt
@@ -0,0 +1,11 @@
+class circulo(raio: Double){
+	fun calcularArea(raio: Double):Double{
+        return 3.14 * raio
+    }
+    
+    fun calcurarPerimetro(raio: Double):Double{
+        return 2 * 3.14 * raio
+    }
+}
+    
+    
